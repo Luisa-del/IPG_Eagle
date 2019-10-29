cb048d5255a5ec0a9722e173c69c846caf97ef22
Antonio Jose Castaneda Gomez <antonio.castanedag@gmail.com>
2019-10-29 11:22:07 +0100

    Some new functions

--- Class_2.R
+++ Class_2.R
@@ -1,1 +1,19 @@
 #Class_2
+
+#install.packages("fun")
+library(fun)
+if(.Platform$OS.type == "windows") x11() else x11(type = "Xlib")
+mine_sweeper()
+
+
+#install.packages("sudoku")
+library(sudoku)
+playSudoku()
+
+# Cheer up Briean!
+devtools::install_github("gaborcsardi/praise")
+library(praise)
+praise()
+
+# Time untl Chitsmas up Briean!
+difftime("2019-12-24",Sys.Date(),units="secs")
